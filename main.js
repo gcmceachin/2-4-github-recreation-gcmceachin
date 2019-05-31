a6eb38bba0d44046a4c786a2c8d1fc664056e03e
gcmceachin <gcmceachin@gmail.com>
2019-05-31 12:34:28 -0400

    github api consolelog gotten

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-$(document).ready(function(){(
+$(document).ready(function() {
 
+'use strict';
 
+const Client_ID = 'Iv1.ea8fd8f633a558f3';
+const Client_Secret ='371f3894fec7f9def91deaae82a40d51fa47f21a';
 
 $.ajax({
-  url: `${BASE_URL}?api_key=${API_KEY}&limit=${limit}&includes=Images:1&keywords=${searchTerm}`,
+  url: `https://api.github.com/users/gcmceachin?client_id=${Client_ID}&client_secret=${Client_Secret}`,
   dataType: 'jsonp',
   method: 'GET',
 
@@ -11,14 +14,25 @@ $.ajax({
     console.log('response', response);
     response = {
       results: response.results
-    }
+    };
     renderHTML(response);
   },
   error: function(xhr) {
-    console.log('uh oh, somthing went wrong', xhr.status)
+    console.log('uh oh, somthing went wrong', xhr.status);
   }
 
 });
 
+function renderHTML(response) {
+    var source = document.getElementById("entry-template").innerHTML;
+    var template = Handlebars.compile(source);
+
+    var context = response;
+
+    var html = template(context);
+
+    $('.row').html(html);
+
+  }
 
 });
